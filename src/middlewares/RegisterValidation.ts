79f6f7337e6b12785202c95917976991c132efcd
raulhenrique-x3 <raulheri43@hotmail.com>
2023-09-07 13:25:46 -0300

    k

--- src/middlewares/RegisterValidation.ts
+++ src/middlewares/RegisterValidation.ts
@@ -7,6 +7,22 @@ function registerValidation(req: Request, res: Response, next: NextFunction) {
     return res.status(400).send({ message: "Preencha todos os campos!" });
   }
 
+  if (!password) {
+    return res.status(400).send({ message: "Por favor preencha a senha" });
+  }
+
+  if (password.length < 8) {
+    return res.status(400).send({ message: 'A senha deve ter no mínimo 8 caracteres.' });
+  }
+
+  if (!/[a-z]/.test(password) || !/[A-Z]/.test(password)) {
+    return res.status(400).send({ message: 'A senha deve conter letras maiúsculas e minúsculas.' });
+  }
+
+  if (!/[!@#$%^&*]/.test(password)) {
+    return res.status(400).send({ message: 'A senha deve conter um caractere especial.' });
+  }
+
   const numberNoCharacter = phone.replace(/\(.*?\)|-/g, "");
 
   req.body.phone = numberNoCharacter;
--- src/routes/user/UserRoute.ts
+++ src/routes/user/UserRoute.ts
@@ -1,3 +1,4 @@
+import registerValidation from "../../middlewares/RegisterValidation";
 import UserController from "../../controllers/User/UserController";
 
 import { Router } from "express";
@@ -5,6 +6,6 @@ import { Router } from "express";
 const userRoutes = Router();
 
 userRoutes.post("/login", UserController.login).bind(UserController);
-userRoutes.post("/register", UserController.register).bind(UserController);
+userRoutes.post("/register", registerValidation, UserController.register).bind(UserController);
 
 export default userRoutes;
