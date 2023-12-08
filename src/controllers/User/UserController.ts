977ded8a4b4ff6d57da349ddd96d9d35c70e5c70
Raul Henrique <raulheri43@hotmail.com>
2023-12-08 00:14:08 -0300

    fix: removing unused import

--- src/controllers/User/UserController.ts
+++ src/controllers/User/UserController.ts
@@ -3,7 +3,6 @@ import { Request, Response } from "express";
 import User from "../../models/User";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import dotenv from "dotenv";
 import { IAddress, IUser } from "@src/@types/user";
 
 class UserController {
